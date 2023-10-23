ef70c1f6a0aba3fa7c6c4bc2c989acb342447086
dexma001 <148747163+dexma001@users.noreply.github.com>
2023-10-23 15:54:54 +0900

    [Gold V] Title: Contact, Time: 112 ms, Memory: 34736 KB -BaekjoonHub

---
+++ 백준/Gold/1013. Contact/Contact.py
@@ -0,0 +1,12 @@
+import re
+import sys
+input = sys.stdin.readline
+
+p = re.compile('(100+1+|01)+')
+
+for i in range(int(input())):
+    s = input().strip()
+    if p.fullmatch(s):
+        print('YES')
+    else:
+        print('NO')
