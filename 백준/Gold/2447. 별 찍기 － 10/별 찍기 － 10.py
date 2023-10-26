fcd9e06caabe52fe6b888d9aae36dd34da768ff9
dexma001 <148747163+dexma001@users.noreply.github.com>
2023-10-26 16:11:42 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 1040 ms, Memory: 69464 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,22 @@
+import copy
+
+n = int(input())
+n1 = copy.deepcopy(n)
+li = [['*' for x in range(n)] for y in range(n)]
+m = 0
+
+while n != 1:
+    n = n / 3
+    m += 1
+
+for i in range(m, 0, -1):
+    per = int(3**i)
+    space = int(3**(i-1))
+    for j in range(space, n1, per):
+        for k in range(space, n1, per):
+            for p in range(j, j+space):
+                for q in range(k, k+space):
+                    li[p][q] = ' '
+
+for i in range(len(li)):
+    print(''.join(li[i]))
