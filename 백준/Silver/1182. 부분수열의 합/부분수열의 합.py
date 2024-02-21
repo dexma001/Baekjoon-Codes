33e5302814192e9905e594e6a8e6d35b61f987fa
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-02-21 15:37:29 +0900

    [Silver II] Title: 부분수열의 합, Time: 276 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,28 @@
+# 1208
+
+import sys
+input = sys.stdin.readline
+
+n, s = map(int, input().split())
+n_arr = list(map(int, input().split()))
+cnt = 0
+
+
+def brute_force(idx, arr):
+    global cnt
+
+    if idx >= n:
+        return -1
+
+    arr += n_arr[idx]
+
+    if arr == s:
+        cnt += 1
+
+    brute_force(idx+1, arr)
+
+    brute_force(idx+1, arr-n_arr[idx])
+
+
+brute_force(0, 0)
+print(cnt)
