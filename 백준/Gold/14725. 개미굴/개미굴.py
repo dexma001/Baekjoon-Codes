a7d554a040ec7e1e174c1b6fd2c02a54695c4976
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-04-15 16:43:15 +0900

    [Gold III] Title: 개미굴, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.py
+++ 백준/Gold/14725. 개미굴/개미굴.py
@@ -5,12 +5,17 @@ input = sys.stdin.readline
 
 n = int(input())
 all_tree = list()
+all_node = list()
 
 for _ in range(n):
     arr = list(map(str, input().split()))[1:]
     all_tree.append(arr)
+    for i in arr:
+        if i not in all_node:
+            all_node.append(i)
 
 all_tree.sort()
+all_node.sort()
 
 answer = list()
 
@@ -27,6 +32,22 @@ for i in range(len(all_tree)):
                 idx += 1
         for k in range(idx, len(all_tree[i])):
             answer.append('--'*k + all_tree[i][k])
+'''
+    for j in range(1, len(all_tree[i])):
+        node = '--'*(j) + all_tree[i][j]
+        if node in answer[start]:
+            k1 = list(
+                filter(lambda x: answer[start][x] == node, range(len(answer[start]))))
+            k = k1[-1]
+            l = 1
+            while j-l >= 0:
+                if answer[start][k-l] != '--'*(j-l) + all_tree[i][j-l]:
+                    answer[start].append(node)
+                    break
+                l += 1
+        else:
+            answer[start].append(node)
+'''
 
 for i in answer:
     print(i)
