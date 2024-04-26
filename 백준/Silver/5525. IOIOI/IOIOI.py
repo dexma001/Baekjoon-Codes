d7ab8ac63c8ba900e24beafc360d78250703b285
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-04-26 09:17:08 +0900

    [Silver I] Title: IOIOI, Time: 44 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,23 @@
+# 5525 IOIOI
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+pn = str()
+
+for i in range(2*n+1):
+    if i % 2 == 0:
+        pn += 'I'
+    else:
+        pn += 'O'
+
+m = int(input())
+string = str(input().rstrip())
+
+answer = 0
+for j in range(m-(2*n)):
+    if string[j:j+(2*n+1)] == pn:
+        answer += 1
+
+print(answer)
