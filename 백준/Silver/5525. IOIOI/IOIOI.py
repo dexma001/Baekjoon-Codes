b5a61d11692e48730962fe4275200bc61076e201
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-04-26 09:53:44 +0900

    [Silver I] Title: IOIOI, Time: 352 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,23 +1,25 @@
-# 5525 
+# 5525
 
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-pn = str()
-
-for i in range(2*n+1):
-    if i % 2 == 0:
-        pn += 'I'
-    else:
-        pn += 'O'
-
 m = int(input())
 string = str(input().rstrip())
 
 answer = 0
-for j in range(m-(2*n)):
-    if string[j:j+(2*n+1)] == pn:
-        answer += 1
+left, right = 0, 0
+
+while right < m:
+    if string[right:right + 3] == 'IOI':
+        right += 2
+
+        if right - left == 2*n:
+            answer += 1
+            left += 2
+
+    else:
+        right += 1
+        left = right
 
 print(answer)
