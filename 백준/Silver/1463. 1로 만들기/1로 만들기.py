206adce5fa52b650e862144ce73c9e9ace5c6e51
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-04-26 17:12:05 +0900

    [Silver III] Title: 1로 만들기, Time: 560 ms, Memory: 39064 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,18 @@
+# 1463
+
+import sys
+sys.setrecursionlimit(10**8)
+
+n = int(input())
+if n == 1:
+    print(0)
+else:
+    dp = [0] * (n+1)
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + 1
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i//3] + 1)
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i//2] + 1)
+
+    print(dp[n])
