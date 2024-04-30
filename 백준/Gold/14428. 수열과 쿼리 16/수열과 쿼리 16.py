2d31986518f68756e76b1df3b146343667cb1e88
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-04-30 15:54:59 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 5408 ms, Memory: 45812 KB -BaekjoonHub

--- 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.py
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.py
@@ -2,65 +2,61 @@
 
 import sys
 import math
+from collections import defaultdict
 input = sys.stdin.readline
 sys.setrecursionlimit(10**8)
 
 n = int(input())
-arr = [0] + list(map(int, input().split()))
-
 size = math.ceil(math.log(n, 2) + 1)
-segment_tree = list(10**10 for _ in range(2**size))
-size -= 1
+segment_tree = list(10**10 for _ in range(2**(size-1)))
+
+arr = list(map(int, input().split()))
+for i in range(len(arr)):
+    segment_tree.append(arr[i])
+segment_tree.extend([10**10] * (2**(size-1) - n))
 
 
-def segment_setting(left, right, i):
-    if left == right:
-        segment_tree[i] = arr[left]
-        return segment_tree[i]
-    mid = (left + right) // 2
-    segment_tree[i] = min(segment_setting(left, mid, i*2),
-                          segment_setting(mid+1, right, i*2+1))
-    return segment_tree[i]
+for i in range(2**(size-1)-1, 0, -1):
+    segment_tree[i] = min(segment_tree[i << 1], segment_tree[i << 1 | 1])
 
 
-segment_setting(1, n, 1)
+def segment_update(idx, diff):
+    node = idx + 2**(size-1) - 1
+    segment_tree[node] = diff
+    while node >> 1 != 0:
+        segment_tree[node >> 1] = min(segment_tree[node],
+                                      segment_tree[node ^ 1])
+        node >>= 1
 
 
-def segment_update(start, end, idx, diff, i):
-    if start > idx or idx > end:
-        return segment_tree[i]
-    if start == end:
-        if start == idx:
-            segment_tree[i] = diff
-        return segment_tree[i]
-    mid = (start + end) // 2
-    if idx <= mid:
-        segment_tree[i] = min(segment_tree[i*2+1],
-                              segment_update(start, mid, idx, diff, i*2))
-    else:
-        segment_tree[i] = min(
-            segment_tree[i*2], segment_update(mid+1, end, idx, diff, i*2+1))
-    return segment_tree[i]
+def segment_search(left, right):
+    leftnode = left + 2**(size-1) - 1
+    rightnode = right + 2**(size-1) - 1
+    answer = 10**10
+
+    while leftnode <= rightnode:
+        if leftnode % 2 == 1:
+            answer = min(answer, segment_tree[leftnode])
+            leftnode += 1
+        if rightnode % 2 == 0:
+            answer = min(answer, segment_tree[rightnode])
+            rightnode -= 1
+
+        leftnode >>= 1
+        rightnode >>= 1
+
+    return answer
 
 
-def segment_search(left, right, nodeleft, noderight, i):
-    if right < nodeleft or left > noderight:
-        return 10**10
-    if left <= nodeleft and noderight <= right:
-        return segment_tree[i]
-
-    mid = (nodeleft + noderight) // 2
-    return min(segment_search(left, right, nodeleft, mid, i*2), segment_search(left, right, mid+1, noderight, i*2+1))
-
-
-for _ in range(int(input())):
+m = int(input())
+for _ in range(m):
     x, y, z = map(int, input().split())
     if x == 1:
-        arr[y] = z
-        segment_update(1, n, y, z, 1)
+        arr[y-1] = z
+        segment_update(y, z)
     else:
-        a = (segment_search(y, z, 1, n, 1))
-        for i in range(y, z+1):
+        a = segment_search(y, z)
+        for i in range(y-1, z):
             if arr[i] == a:
-                print(i)
+                print(i+1)
                 break
