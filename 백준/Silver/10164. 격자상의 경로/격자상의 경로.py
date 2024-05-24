6ac5e217402f6c986e0a1822af4d1760ccfc9fed
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-05-24 17:13:38 +0900

    [Silver II] Title: 격자상의 경로, Time: 44 ms, Memory: 33372 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
@@ -8,9 +8,19 @@ fac = math.factorial
 n, m, s = map(int, input().split())
 
 if s == 0:
-    print(int(fac(n+m-2) / fac(n-1) / fac(m-1)))
+    if n == 1 or m == 1:
+        print(1)
+    else:
+        print(int(fac(n+m-2) / fac(n-1) / fac(m-1)))
 else:
-    x = s//m + 1
-    y = s % m
-    print(int(fac(x+y-2) / fac(x-1) / fac(y-1)) *
-          int(fac((n-x) + (m-y)) / fac((n-x)) / fac(m-y)))
+    if n == 1 or m == 1:
+        print(1)
+    else:
+        if s % m == 0:
+            x = s//m
+            y = m
+        else:
+            x = s//m + 1
+            y = s % m
+        print(int(fac(x+y-2) / fac(x-1) / fac(y-1)) *
+              int(fac((n-x) + (m-y)) / fac((n-x)) / fac(m-y)))
