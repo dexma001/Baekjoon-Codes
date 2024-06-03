e75341ec06edb2ebf69cfed04a3be392b92f59df
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-06-03 16:33:28 +0900

    [Gold IV] Title: 작업, Time: 1100 ms, Memory: 65120 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.py
+++ 백준/Gold/2056. 작업/작업.py
@@ -5,23 +5,23 @@ input = sys.stdin.readline
 
 n = int(input())
 time = [0] * (n+1)
+edge_in = [0] * (n+1)
 previous = [[]] * (n+1)
 
 for i in range(n):
     arr = list(map(int, input().split()))
     time[i+1] = arr[0]
+    edge_in[i+1] = arr[1]
     previous[i+1] = arr[2:]
 
 edge = list([] for _ in range(n+1))
-edge_in = list(0 for _ in range(n+1))
-
 for i in range(1, n+1):
     for j in previous[i]:
         edge[j].append(i)
         edge[i].append(j)
-        edge_in[i] += 1
 
 visited = [False] * (n+1)
+
 stack = list()
 for i in range(1, n+1):
     if edge_in[i] == 0:
@@ -30,15 +30,17 @@ for i in range(1, n+1):
 dp = [0] * (n+1)
 while stack:
     for _ in range(len(stack)):
-        i = stack.pop(0)
-        if visited[i] == True:
+        temp = stack.pop(0)
+        if visited[temp] == True:
             continue
-        visited[i] = True
-        for k in previous[i]:
-            dp[i] = max(dp[i], time[k])
-        dp[i] += time[i]
-        time[i] = dp[i]
-        for j in edge[i]:
+
+        visited[temp] = True
+        for k in previous[temp]:
+            dp[temp] = max(dp[temp], time[k])
+        dp[temp] += time[temp]
+        time[temp] = dp[temp]
+
+        for j in edge[temp]:
             if visited[j] == False:
                 edge_in[j] -= 1
             if edge_in[j] == 0:
