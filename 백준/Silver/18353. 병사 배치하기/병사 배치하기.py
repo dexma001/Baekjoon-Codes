968538f7f81cf94bfb732a10b9b251dfe8266532
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-06-04 11:40:32 +0900

    [Silver II] Title: 병사 배치하기, Time: 744 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,16 @@
+# 18353
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [0] + list(map(int, input().split()))
+dp = [0] * (n+1)
+
+for i in range(1, n+1):
+    dp[i] = 1
+    for j in range(1, i):
+        if arr[j] > arr[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
