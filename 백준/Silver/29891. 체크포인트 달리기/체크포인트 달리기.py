daa9d6b53b49625dd86bcd52672e008189e83537
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-06-10 10:48:50 +0900

    [Silver IV] Title: 체크포인트 달리기, Time: 176 ms, Memory: 39320 KB, Score: 70 point -BaekjoonHub

--- 백준/Silver/29891. 체크포인트 달리기/체크포인트 달리기.py
+++ 백준/Silver/29891. 체크포인트 달리기/체크포인트 달리기.py
@@ -4,17 +4,30 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-arr = list()
+arr_plus = list()
+arr_minus = list()
 
 for _ in range(n):
-    arr.append(int(input()))
-arr.sort()
+    temp = int(input())
+    if temp >= 0:
+        arr_plus.append(temp)
+    else:
+        arr_minus.append(-temp)
+
+arr_plus.sort(reverse=True)
+arr_minus.sort(reverse=True)
 
 answer = 0
-while arr:
-    answer += abs(arr[-1]) * 2
-    for _ in range(m):
-        if arr:
-            arr.pop()
+while len(arr_plus) >= m:
+    answer += abs(arr_plus[0]) * 2
+    arr_plus = arr_plus[m:]
+if len(arr_plus) != 0:
+    answer += arr_plus[0] * 2
+
+while len(arr_minus) >= m:
+    answer += abs(arr_minus[0]) * 2
+    arr_minus = arr_minus[m:]
+if len(arr_minus) != 0:
+    answer += arr_minus[0] * 2
 
 print(answer)
