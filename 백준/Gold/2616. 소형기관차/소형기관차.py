be34ae65d8830a66537981ec7fa1b1fc3e94c0cf
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-06-18 17:09:19 +0900

    [Gold III] Title: 소형기관차, Time: 116 ms, Memory: 35344 KB -BaekjoonHub

--- 백준/Gold/2616. 소형기관차/소형기관차.py
+++ 백준/Gold/2616. 소형기관차/소형기관차.py
@@ -1,32 +1,29 @@
 # 2616
-# 기관차는 모두 3대
 
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = [0]+list(map(int, input().split()))
-carry = int(input())
+arr = [0] + list(map(int, input().split()))
+max_carry = int(input())
 
-perfix_sum = list(0 for _ in range(n-carry+2))
-perfix_sum[1] = sum(arr[1:carry+1])
-for i in range(2, n-carry+2):
-    perfix_sum[i] = perfix_sum[i-1] - arr[i-1] + arr[i+carry-1]
+perfix_sum = list(0 for _ in range(n-max_carry+2))
+perfix_sum[1] = sum(arr[1:max_carry+1])
+for i in range(2, n-max_carry+2):
+    perfix_sum[i] = perfix_sum[i-1] - arr[i-1] + arr[i+max_carry-1]
 
-dp = list([0]*(n-carry+2) for _ in range(4))
+dp = list([0] * (n-max_carry+2) for _ in range(4))
 
-dp[3][n-carry+1] = perfix_sum[-1]
-for i in range(n-carry, 0, -1):
-    dp[3][i] = max(dp[3][i+1], perfix_sum[i])
-
-dp[2][n-carry*2+1] = perfix_sum[n-carry*2+1] + dp[3][-1]
-for i in range(n-carry*2, 0, -1):
-    dp[2][i] = max(dp[2][i+1], dp[3][i+carry] + perfix_sum[i])
-
-
-dp[1][n-carry*3+1] = perfix_sum[n-carry*3+1] + dp[2][n-carry*2+1]
-for i in range(n-carry*3, 0, -1):
-    dp[1][i] = max(dp[1][i+1], dp[2][i+carry] + perfix_sum[i])
-
+for i in range(3, 0, -1):
+    if i == 3:
+        dp[i][n-(max_carry*(4-i))+1] = perfix_sum[-1]
+    else:
+        dp[i][n-(max_carry*(4-i))+1] = perfix_sum[n -
+                                                  (max_carry*(4-i))+1] + dp[i+1][n-max_carry*(3-i)+1]
+    for j in range(n-(max_carry)*(4-i), 0, -1):
+        if i == 3:
+            dp[i][j] = max(dp[i][j+1], perfix_sum[j])
+        else:
+            dp[i][j] = max(dp[i][j+1], dp[i+1][j+max_carry] + perfix_sum[j])
 
 print(dp[1][1])
