902109d3436d349630007409ac2fba30212802b5
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-06-19 12:02:36 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.py
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.py
@@ -1,38 +1,24 @@
-# 17070
+# 17069
 
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
 
 n = int(input())
-li = list()
-answer = 0
-
+arr = [[0]*(n+1)]
 for _ in range(n):
-    li.append(list(map(int, input().split())))
+    temp = [0] + list(map(int, input().split()))
+    arr.append(temp)
 
-dp_arr = [[[0, 0, 0] for _ in range(n)] for _ in range(n)]
-# 가 / 세 / 대
-dp_arr[0][1][0] += 1
+dp = list(list([0, 0, 0] for _ in range(n+1)) for _ in range(n+1))  # 가/세/대
+dp[1][2] = [0, 0, 1]
+for i in range(3, n+1):
+    for j in range(1, n+1):
+        if arr[j][i] != 1:
+            if arr[j-1][i] != 1:
+                dp[j][i][1] = dp[j-1][i][1] + dp[j-1][i][2]
+            if arr[j][i-1] != 1:
+                dp[j][i][0] = dp[j][i-1][0] + dp[j][i-1][2]
+            if arr[j-1][i] != 1 and arr[j][i-1] != 1 and arr[j-1][i-1] != 1:
+                dp[j][i][2] = sum(dp[j-1][i-1])
 
-for x in range(1, n):
-    if li[0][x] != 1:
-        dp_arr[0][x][0] += dp_arr[0][x-1][0]
-
-for i in range(1, n):
-    for j in range(1, n):
-        if li[i][j] != 1:
-            if li[i-1][j] & li[i][j-1] == 1:
-                continue
-            elif li[i-1][j] == 0 and li[i][j-1] == 1:
-                dp_arr[i][j][1] = dp_arr[i-1][j][1] + dp_arr[i-1][j][2]
-            elif li[i-1][j] == 1 and li[i][j-1] == 0:
-                dp_arr[i][j][0] = dp_arr[i][j-1][0] + dp_arr[i][j-1][2]
-            else:
-                dp_arr[i][j][2] = dp_arr[i-1][j-1][0] + \
-                    dp_arr[i-1][j-1][1] + dp_arr[i-1][j-1][2]
-                dp_arr[i][j][0] = dp_arr[i][j-1][0] + dp_arr[i][j-1][2]
-                dp_arr[i][j][1] = dp_arr[i-1][j][1] + dp_arr[i-1][j][2]
-
-
-print(dp_arr[n-1][n-1][0] + dp_arr[n-1][n-1][1] + dp_arr[n-1][n-1][2])
+print(sum(dp[n][n]))
