063b0efd05c421e5714c0cdda2edc209153aa21f
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-06-21 09:04:04 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,7 @@
+arr = dict()
+arr['NLCS'] = 'North London Collegiate School'
+arr['BHA'] = 'Branksome Hall Asia'
+arr['KIS'] = 'Korea International School'
+arr['SJA'] = 'St. Johnsbury Academy'
+
+print(arr[str(input())])
