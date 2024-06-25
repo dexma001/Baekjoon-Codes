987118b7fa564707edc29acae591b0f91f2a2a94
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-06-25 10:11:32 +0900

    [Gold V] Title: 수강 과목, Time: 540 ms, Memory: 189476 KB -BaekjoonHub

---
+++ 백준/Gold/17845. 수강 과목/수강 과목.py
@@ -0,0 +1,24 @@
+# 17845
+# Same as 14728
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+arr = list()
+for _ in range(k):
+    arr.append(list(map(int, input().split())))
+arr.sort(key=lambda x: x[1])
+
+dp = list(list(0 for _ in range(n+1)) for _ in range(k+1))
+
+for i in range(k+1):
+    for j in range(n+1):
+        if i == 0 or j == 0:
+            dp[i][j] = 0
+        elif arr[i-1][1] <= j:
+            dp[i][j] = max(dp[i-1][j], arr[i-1][0] + dp[i-1][j-arr[i-1][1]])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[k][n])
