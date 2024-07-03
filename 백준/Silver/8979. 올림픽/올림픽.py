0aff7a55a551407d5d4d46b57422448aaf205963
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-07-03 12:00:27 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -9,12 +9,27 @@ arr = list()
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
-arr.sort(key=lambda x: [x[1], x[2], x[3]])
+arr.sort(key=lambda x: [x[1], x[2], x[3]], reverse=True)
 
-answer = 0
-for i in range(n):
-    if arr[i][0] == k:
-        break
-    answer += 1
+
+answer = 1
+if arr[0][0] == k:
+    1
+else:
+    temp = 1
+    for i in range(1, n):
+        if arr[i][0] == k:
+            if arr[i-1][1:] == arr[i][1:]:
+                break
+            else:
+                answer += temp
+                break
+
+        if arr[i-1][1:] == arr[i][1:]:
+            temp += 1
+        else:
+            answer += temp
+            temp = 1
+
 
 print(answer)
