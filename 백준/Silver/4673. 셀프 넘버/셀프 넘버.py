dc6f6c8966e4daf196b709a216fad90b595d4a16
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-07-03 17:48:03 +0900

    [Silver V] Title: 셀프 넘버, Time: 316 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -7,13 +7,15 @@ for i in range(1, 10001):
     if det_arr[i] == 0:
         print(i)
 
-    while True:
-        temp = 0
-        for j in str(i):
-            temp += int(j)
-        temp += i
-        if temp < 10001:
-            det_arr[temp] = 1
-            i = temp
-        else:
-            break
+        while True:
+            temp = 0
+            for j in str(i):
+                temp += int(j)
+            temp += i
+            if temp < 10001:
+                det_arr[temp] = 1
+                i = temp
+            else:
+                break
+    else:
+        continue
