b82ff42c4e932e9dbc9315a55412f763bba24d87
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-07-23 15:28:22 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 1012 ms, Memory: 249824 KB -BaekjoonHub

--- 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.py
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.py
@@ -8,20 +8,20 @@ n, m, k = map(int, input().split())
 
 arr = [0] + list(int(input()) for _ in range(n))
 
-size = math.ceil(math.log2(n))+1
+size = math.ceil(math.log2(n)) + 1
 size_ = 1 << size
 seg = list(0 for _ in range(size_+1))
 lazy_seg = list(0 for _ in range(size_+1))
 
 
-def make_seg(idx, start, end):
-    if start == end:
-        seg[idx] = arr[start]
+def make_seg(idx, left, right):
+    if left == right:
+        seg[idx] = arr[left]
         return seg[idx]
 
-    mid = (start+end)//2
-    l1 = make_seg(idx*2, start, mid)
-    r1 = make_seg(idx*2+1, mid+1, end)
+    mid = (left+right)//2
+    l1 = make_seg(idx*2, left, mid)
+    r1 = make_seg(idx*2+1, mid+1, right)
     seg[idx] = l1+r1
     return seg[idx]
 
@@ -29,7 +29,7 @@ def make_seg(idx, start, end):
 make_seg(1, 1, n)
 
 
-def update_lazy(idx, start, end):
+def lazy_update(idx, start, end):
     if lazy_seg[idx] != 0:
         seg[idx] += (end-start+1)*lazy_seg[idx]
 
@@ -40,38 +40,38 @@ def update_lazy(idx, start, end):
         lazy_seg[idx] = 0
 
 
-def update_seg(idx, start, end, left, right, val):
-    update_lazy(idx, start, end)
+def find_seg(idx, left, right, start, end):
+    lazy_update(idx, left, right)
 
-    if left > end or right < start:
+    if start > right or end < left:
+        return 0
+
+    if start <= left and right <= end:
+        return seg[idx]
+
+    mid = (left+right)//2
+    l1 = find_seg(idx*2, left, mid, start, end)
+    r1 = find_seg(idx*2+1, mid+1, right, start, end)
+    return (l1+r1)
+
+
+def update_seg(idx, left, right, start, end, val):
+    lazy_update(idx, left, right)
+
+    if end < left or right < start:
         return
 
-    if left <= start and end <= right:
-        seg[idx] += (end-start+1)*val
-        if start != end:
+    if start <= left and right <= end:
+        seg[idx] += (right-left+1)*val
+        if left != right:
             lazy_seg[idx*2] += val
             lazy_seg[idx*2+1] += val
         return
 
-    mid = (start+end)//2
-    update_seg(idx*2, start, mid, left, right, val)
-    update_seg(idx*2+1, mid+1, end, left, right, val)
-    seg[idx] = seg[idx*2]+seg[idx*2+1]
-
-
-def find_seg(idx, start, end, left, right):
-    update_lazy(idx, start, end)
-
-    if right < start or left > end:
-        return 0
-
-    if left <= start and end <= right:
-        return seg[idx]
-
-    mid = (start+end)//2
-    l1 = find_seg(idx*2, start, mid, left, right)
-    r1 = find_seg(idx*2+1, mid+1, end, left, right)
-    return l1+r1
+    mid = (left+right)//2
+    update_seg(idx*2, left, mid, start, end, val)
+    update_seg(idx*2+1, mid+1, right, start, end, val)
+    seg[idx] = seg[idx*2] + seg[idx*2+1]
 
 
 for _ in range(m+k):
