4371a173c2d0d5d4cd591c5284a16ab402926e32
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-08-06 14:59:06 +0900

    [Gold IV] Title: 전화번호 목록, Time: 152 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -3,58 +3,14 @@
 import sys
 input = sys.stdin.readline
 
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(input().rstrip() for _ in range(n))
+    arr.sort()
 
-class Node(object):
-    def __init__(self, key, data=None):
-        self.key = key
-        self.data = data
-        self.children = {}
-
-
-class Trie(object):
-    def __init__(self):
-        self.head = Node(None)
-
-    def insert(self, string):
-        curr_node = self.head
-
-        for char in string:
-            if char not in curr_node.children:
-                curr_node.children[char] = Node(char)
-            curr_node = curr_node.children[char]
-
-        curr_node.data = True
-
-    def search(self, string):
-        curr_node = self.head
-
-        for char in string:
-            if char not in curr_node.children:
-                return True
-            curr_node = curr_node.children[char]
-
-            if curr_node.data:
-                return False
-
-        return True
-
-
-if __name__ == "__main__":
-    for _ in range(int(input())):
-        n = int(input())
-        arr = list()
-        for _ in range(n):
-            temp = input().rstrip()
-            arr.append((len(temp), temp))
-
-        arr.sort()
-
-        data = Trie()
-
-        for _, number in arr:
-            if data.search(number) == False:
-                print('NO')
-                break
-            data.insert(number)
-        else:
-            print('YES')
+    for i, j in zip(arr, arr[1:]):
+        if j.startswith(i):
+            print("NO")
+            break
+    else:
+        print('YES')
