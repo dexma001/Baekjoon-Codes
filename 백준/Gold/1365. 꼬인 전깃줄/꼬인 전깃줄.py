51848f04f74c9d6614f48762360c7ad183429b8c
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-08-30 17:54:37 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 80 ms, Memory: 44104 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.py
@@ -0,0 +1,19 @@
+# 1365
+
+import sys
+import bisect
+input = sys.stdin.readline
+
+n = int(input())
+arr = [0] + list(map(int, input().split()))
+
+answer = list()
+
+for i in arr[1:]:
+    if not answer or i > answer[-1]:
+        answer.append(i)
+    else:
+        index = bisect.bisect_left(answer, i)
+        answer[index] = i
+
+print(n - len(answer))
