fbd1836eb0919369de9d1b18ec849a05d209a9d2
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-10-10 17:18:42 +0900

    [Gold I] Title: 택배, Time: 232 ms, Memory: 36320 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.py
+++ 백준/Gold/8980. 택배/택배.py
@@ -10,7 +10,7 @@ arr = list()
 for _ in range(m):
     arr.append(list(map(int, input().split())))
 
-arr.sort(key=lambda x: [x[0], x[1], x[2]])
+arr.sort(key=lambda x: [x[0], x[1], -x[2]])
 
 answer = 0
 truck = list()
@@ -26,29 +26,76 @@ while arr:
             truck_total -= j1
 
     truck_location = i
-    if truck_total + k <= c:
+    if not truck:
+        heapq.heappush(truck, [j, min(c, k)])
+        truck_total = min(c, k)
+    elif truck_total + k <= c:
         truck_total += k
         heapq.heappush(truck, [j, k])
-    elif truck_total != c:
-        temp = c - truck_total
-        truck_total = c
-        heapq.heappush(truck, [j, temp])
-    else:
-        temp_loc = 0
-        temp_loc_index = -1
-        for t in range(len(truck)):
-            p, q = truck[t]
-            if p > j:
-                if p > temp_loc:
-                    temp_loc = p
-                    temp_loc_index = t
-        if temp_loc_index != -1:
-            truck[temp_loc_index][1] -= k
-            truck.append([j, k])
-            heapq.heapify(truck)
+    else:  # truck_total + k > c
+        truck.sort(key=lambda x: [x[0], -x[1]])
+        if truck[-1][0] <= j and c - truck_total > 0:
+            truck.append([j, c - truck_total])
+            truck_total = c
+        else:
+            while truck and truck[-1][0] > j:
+                if c-(truck_total-truck[-1][1]) == k:
+                    truck.pop()
+                    truck.append([j, k])
+                    truck_total = c
+                    break
+                elif c - (truck_total - truck[-1][1]) > k:
+                    break
+                else:
+                    truck_total -= truck[-1][1]
+                    truck.pop()
+
+            if truck_total <= c:
+                if truck and truck[-1][0] > j:
+                    truck[-1][1] = c-(truck_total-truck[-1][1]+k)
+                    truck.append([j, k])
+                    truck_total = c
+                else:
+                    truck.append([j, min(c - truck_total, k)])
+                    truck_total = c
+        heapq.heapify(truck)
 
 while truck:
     ii, jj = heapq.heappop(truck)
     answer += jj
 
 print(answer)
+
+'''
+좋은 반례
+5 40
+3
+1 5 10
+2 4 40
+4 5 40
+answer = 80
+
+4 50
+4
+1 3 15
+1 4 35
+2 3 90
+3 4 80
+answer = 100
+
+4 50
+4
+1 3 35
+1 4 90
+2 3 10
+3 4 30
+answer = 80
+
+4 50
+4
+1 3 35
+1 4 10
+2 3 10
+3 4 30
+answer = 80
+'''
