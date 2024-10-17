438090716fd34e4e9d67f322f9c478b83386bc6e
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-10-17 17:21:32 +0900

    [Platinum IV] Title: 노트북의 주인을 찾아서, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Platinum/1298. 노트북의 주인을 찾아서/노트북의 주인을 찾아서.py
@@ -0,0 +1,36 @@
+# 2188
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+people = list(list() for _ in range(n+1))
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    people[a].append(b)
+
+notebook = list(0 for _ in range(n+1))
+
+
+def bimatch(i):
+    if visited[i]:
+        return False
+    visited[i] = True
+
+    for j in people[i]:
+        if notebook[j] == 0 or bimatch(notebook[j]):
+            notebook[j] = i
+            return True
+    return False
+
+
+for i in range(1, n+1):
+    visited = list(0 for _ in range(n+1))
+    bimatch(i)
+
+answer = 0
+for i in notebook:
+    if i:
+        answer += 1
+print(answer)
