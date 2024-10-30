8ea4443a599353e9733d2a725bfa2b1fd928138b
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-10-30 09:34:21 +0900

    [Gold V] Title: 비요뜨의 징검다리 건너기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/18291. 비요뜨의 징검다리 건너기/비요뜨의 징검다리 건너기.py
@@ -0,0 +1,25 @@
+# 18291
+
+import sys
+input = sys.stdin.readline
+mod = 10**9+7
+sys.setrecursionlimit(10**9+7)
+
+
+def power(a, b):
+    result = 1
+    while b:
+        if b % 2 == 1:
+            result *= a
+            result %= mod
+        a = (a*a) % mod
+        b = b//2
+    return result
+
+
+for _ in range(int(input())):
+    n = int(input())
+    if n == 1:
+        print(1)
+    else:
+        print(power(2, n-2) % mod)
