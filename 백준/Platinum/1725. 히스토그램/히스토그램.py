b58fb42089ceebebba1be439daa90d479c6f69ba
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-11-06 14:41:39 +0900

    [Platinum V] Title: 히스토그램, Time: 156 ms, Memory: 43948 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.py
+++ 백준/Platinum/1725. 히스토그램/히스토그램.py
@@ -1,4 +1,4 @@
-# 1725 - 히스토그램
+# 1725
 
 import sys
 input = sys.stdin.readline
@@ -8,18 +8,17 @@ li = list(int(input()) for _ in range(n))
 
 stack = []
 answer = 0
-
 for i in range(n):
-    idx = i
+    id = i
     while stack and stack[-1][1] > li[i]:
-        idx, height = stack.pop()
-        temp_answer = (i - idx) * height
-        answer = max(answer, temp_answer)
-    stack.append([idx, li[i]])
+        id, height = stack.pop()
+        temp_ans = (i - id) * height
+        answer = max(answer, temp_ans)
+    stack.append([id, li[i]])
 
 while stack:
-    idx, height = stack.pop()
-    temp_answer = (n - idx) * height
-    answer = max(answer, temp_answer)
+    id, height = stack.pop()
+    temp_ans = (n - id) * height
+    answer = max(answer, temp_ans)
 
 print(answer)
