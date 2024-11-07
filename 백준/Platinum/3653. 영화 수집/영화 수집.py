64059c68b26c8699ec709f3186fd6f4d8cae6f04
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-11-07 16:29:26 +0900

    [Platinum IV] Title: 영화 수집, Time: 3572 ms, Memory: 235708 KB -BaekjoonHub

--- 백준/Platinum/3653. 영화 수집/영화 수집.py
+++ 백준/Platinum/3653. 영화 수집/영화 수집.py
@@ -4,6 +4,45 @@ import math
 import sys
 input = sys.stdin.readline
 
+
+def make_seg(idx, left, right):
+    if left == right:
+        seg_tree[idx] = arr[left]
+    else:
+        mid = (left+right)//2
+        make_seg(idx*2, left, mid)
+        make_seg(idx*2+1, mid+1, right)
+        seg_tree[idx] = seg_tree[idx*2] + seg_tree[idx*2+1]
+    return seg_tree[idx]
+
+
+def find_seg(idx, left, right, start, end):
+    if start > right or end < left:
+        return 0
+    if start <= left and right <= end:
+        return seg_tree[idx]
+
+    mid = (left+right)//2
+    l1 = find_seg(idx*2, left, mid, start, end)
+    r1 = find_seg(idx*2+1, mid+1, right, start, end)
+    return l1 + r1
+
+
+def update_seg(idx, left, right, new_idx, value):
+    if right < new_idx or left > new_idx:
+        return
+
+    if left == right:
+        seg_tree[idx] = value
+
+    else:
+        mid = (left+right)//2
+        update_seg(idx*2, left, mid, new_idx, value)
+        update_seg(idx*2+1, mid+1, right, new_idx, value)
+        seg_tree[idx] = seg_tree[idx*2] + seg_tree[idx*2+1]
+    return seg_tree[idx]
+
+
 for _ in range(int(input())):
     n, m = map(int, input().split())
     arr = [1]*(n+1)+[0]*(m)
@@ -14,45 +53,11 @@ for _ in range(int(input())):
     size_ = 1 << size
     seg_tree = list(0 for _ in range(size_+1))
 
-    def make_seg(idx, left, right):
-        if left == right:
-            seg_tree[idx] = arr[left]
-        else:
-            mid = (left+right)//2
-            make_seg(idx*2, left, mid)
-            make_seg(idx*2+1, mid+1, right)
-            seg_tree[idx] = seg_tree[idx*2] + seg_tree[idx*2+1]
-        return seg_tree[idx]
-
     make_seg(1, 1, n+m)
-
-    def find_seg(idx, left, right, start, end):
-        if start > right or end < left:
-            return 0
-        if start <= left and right <= end:
-            return seg_tree[idx]
-
-        mid = (left+right)//2
-        l1 = find_seg(idx*2, left, mid, start, end)
-        r1 = find_seg(idx*2+1, mid+1, right, start, end)
-        return l1 + r1
-
-    def update_seg(idx, left, right, new_idx, value):
-        if right < new_idx or left > new_idx:
-            return
-
-        if left == right:
-            seg_tree[idx] = value
-
-        else:
-            mid = (left+right)//2
-            update_seg(idx*2, left, mid, new_idx, value)
-            update_seg(idx*2+1, mid+1, right, new_idx, value)
-            seg_tree[idx] = seg_tree[idx*2] + seg_tree[idx*2+1]
-        return seg_tree[idx]
 
     answer = list()
     new_idx = n
+
     for i in want_to_watch:
         index = dvd_idx[i]
         answer.append(find_seg(1, 1, n+m, index+1, new_idx))
@@ -60,4 +65,5 @@ for _ in range(int(input())):
         new_idx += 1
         update_seg(1, 1, n+m, new_idx, 1)
         dvd_idx[i] = new_idx
+
     print(*answer)
