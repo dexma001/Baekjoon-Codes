18fcb523fee647d1607086e6cffdcbb5e2f068da
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-11-07 16:47:01 +0900

    [Bronze I] Title: Favorite Number, Time: 1400 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Bronze/10570. Favorite Number/Favorite Number.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+
+for _ in range(int(input())):
+    answer = defaultdict(int)
+    for _ in range(int(input())):
+        answer[int(input())] += 1
+    temp = list(answer.items())
+    temp.sort(key=lambda x: (-x[1], x[0]))
+    print(temp[0][0])
