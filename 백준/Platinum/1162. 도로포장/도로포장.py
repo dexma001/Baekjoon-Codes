08192369e0f0fd0e36cbe5b1efe3ba6edbba5038
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-11-22 09:57:50 +0900

    [Platinum V] Title: 도로포장, Time: 1224 ms, Memory: 190924 KB -BaekjoonHub

--- 백준/Platinum/1162. 도로포장/도로포장.py
+++ 백준/Platinum/1162. 도로포장/도로포장.py
@@ -3,41 +3,38 @@
 import heapq
 import sys
 input = sys.stdin.readline
+inf = sys.maxsize
 
 n, m, k = map(int, input().split())
 road = list(list() for _ in range(n+1))
+
 for _ in range(m):
     a, b, c = map(int, input().split())
     road[a].append([b, c])
     road[b].append([a, c])
-inf = sys.maxsize
 
-dp = list(list(inf for _ in range(n+1)) for _ in range(k+1))
+dp = list(list(inf for _ in range(k+1)) for _ in range(n+1))
 
 for i in range(k+1):
-    dp[i][1] = 0
+    dp[1][i] = 9
 
 q = list()
 heapq.heappush(q, (0, 1, 0))
 
 while q:
     now_dist, now, p = heapq.heappop(q)
-    if dp[p][now] < now_dist:
+    if dp[now][p] < now_dist:
         continue
 
     if p + 1 <= k:
         for x, y in road[now]:
-            if dp[p+1][x] > now_dist:
-                dp[p+1][x] = now_dist
+            if dp[x][p+1] > now_dist:
+                dp[x][p+1] = now_dist
                 heapq.heappush(q, (now_dist, x, p+1))
 
     for x, y in road[now]:
-        if dp[p][x] > now_dist + y:
-            dp[p][x] = now_dist + y
-            heapq.heappush(q, (dp[p][x], x, p))
+        if dp[x][p] > now_dist + y:
+            dp[x][p] = now_dist + y
+            heapq.heappush(q, (now_dist+y, x, p))
 
-ans = inf
-for i in range(k+1):
-    ans = min(ans, dp[i][-1])
-
-print(ans)
+print(min(dp[n]))
