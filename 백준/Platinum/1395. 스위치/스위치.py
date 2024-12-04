f31e6af154214ebf6cfae98327c5207db4c49fe9
dexma001 <148747163+dexma001@users.noreply.github.com>
2024-12-04 17:47:40 +0900

    [Platinum III] Title: 스위치, Time: 2300 ms, Memory: 37548 KB -BaekjoonHub

---
+++ 백준/Platinum/1395. 스위치/스위치.py
@@ -0,0 +1,57 @@
+# 1395
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+seg = list(0 for _ in range(4*n+1))
+lazy = list(0 for _ in range(4*n+1))
+
+
+def lazy_update(idx, left, right):
+    if lazy[idx] % 2:
+        seg[idx] = (right-left+1) - seg[idx]
+        if left != right:
+            lazy[idx*2] += 1
+            lazy[idx*2+1] += 1
+    lazy[idx] = 0
+
+
+def update_seg(idx, left, right, start, end):
+    lazy_update(idx, left, right)
+    if end < left or right < start:
+        return
+
+    if start <= left and right <= end:
+        seg[idx] = (right-left+1) - seg[idx]
+        if left != right:
+            lazy[idx*2] += 1
+            lazy[idx*2+1] += 1
+        return
+
+    mid = (left + right)//2
+    update_seg(idx*2, left, mid, start, end)
+    update_seg(idx*2+1, mid+1, right, start, end)
+    seg[idx] = seg[idx*2] + seg[idx*2+1]
+    return
+
+
+def find_seg(idx, left, right, start, end):
+    lazy_update(idx, left, right)
+    if end < left or right < start:
+        return 0
+
+    if start <= left and right <= end:
+        return seg[idx]
+
+    mid = (left+right)//2
+    return find_seg(idx*2, left, mid, start, end) + find_seg(idx*2+1, mid+1, right, start, end)
+
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    if a == 0:
+        update_seg(1, 1, n, b, c)
+    else:
+        print(find_seg(1, 1, n, b, c))
