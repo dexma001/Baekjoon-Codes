4b87abf07427f5b2b175157f0ee41e6fb35bc223
dexma001 <148747163+dexma001@users.noreply.github.com>
2025-01-13 22:45:10 +0900

    [Silver II] Title: 초콜릿 식사, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.py
+++ 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.py
@@ -9,12 +9,13 @@ temp = 1 << n
 answer.append(temp)
 
 cnt = 0
-while k != 0:
+while True:
     if temp <= k:
         k -= temp
 
     if k == 0:
         answer.append(cnt)
+        break
     cnt += 1
     temp //= 2
 
