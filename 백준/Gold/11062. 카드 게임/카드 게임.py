5f90198a966fd5930bcf1ea647c1748d5bc413af
dexma001 <148747163+dexma001@users.noreply.github.com>
2025-05-13 17:00:17 +0900

    [Gold III] Title: 카드 게임, Time: 56 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/11062. 카드 게임/카드 게임.py
+++ 백준/Gold/11062. 카드 게임/카드 게임.py
@@ -1,28 +1,62 @@
-#11062
+# idea from https://hijkl2e.tistory.com/119
 
 import sys
 input = sys.stdin.readline
 
-def solve(left, right, turn):
-        if left > right:
-            return 0
-        
-        if dp[left][right]:
-            return dp[left][right]
-        
-        if turn == 1:
-            dp[left][right] = max(arr[left] + solve(left+1, right, 0), arr[right] + solve(left, right-1, 0))
-            return dp[left][right]
-        
-        else:
-            dp[left][right] = min(solve(left, right-1, 1), solve(left+1, right, 1))
-            return dp[left][right]
 for _ in range(int(input())):
     leng = int(input())
-    arr = [0] + list(map(int, input().split()))
-    
-    dp = list(list(0 for _ in range(leng +1)) for _ in range(leng+1))
-    solve(1, leng, 1)
-    print(dp[1][leng])
-    
-    
+    arr = list(map(int, input().split()))
+    all_sum = sum(arr)
+
+    if leng < 3:
+        print(max(arr))
+        continue
+
+    temp = list()
+    i = 2
+    temp.append(arr[0])
+    temp.append(arr[1])
+
+    while i < leng:
+        if len(temp) < 2:
+            temp.append(arr[i])
+            i += 1
+            continue
+
+        if temp[-1] >= max(arr[i], temp[-2]):
+            t1 = temp.pop()
+            t2 = temp.pop()
+            arr[i] = arr[i] + t2 - t1
+
+        else:
+            temp.append(arr[i])
+            i += 1
+
+    l_r = [0, 0]
+    turn = 0
+
+    left = 0
+    right = len(temp)-1
+
+    while left <= right:
+        if temp[left] > temp[right]:
+            l_r[turn] += temp[left]
+            left += 1
+        elif temp[left] < temp[right]:
+            l_r[turn] += temp[right]
+            right -= 1
+        else:
+            if right - left <= 2:
+                l_r[turn] += temp[left]
+                left += 1
+            else:
+                if temp[left+1] <= temp[right-1]:
+                    l_r[turn] += temp[left]
+                    left += 1
+                else:
+                    l_r[turn] += temp[right]
+                    right -= 1
+
+        turn ^= 1
+
+    print((all_sum+(l_r[0]-l_r[1]))//2)
