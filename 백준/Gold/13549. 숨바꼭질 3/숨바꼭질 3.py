953942c4d033fef2c4eebea84732bf21e6faf476
dexma001 <148747163+dexma001@users.noreply.github.com>
2025-05-19 10:24:58 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 356 ms, Memory: 124224 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,29 +1,28 @@
-#13549
+# 13549
 
-import sys
 import heapq
+import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n, k = map(int, input().split())
 
-if n>= m:
-    print(n-m)
-
+if n >= k:
+    print(n-k)
 else:
-    dijk = list(10**8 for _ in range(100001))
-    stack = list()
-    heapq.heappush(stack, (0, n))
+    dp = list(10**9 for _ in range(100001))
+    arr = list()
+    heapq.heappush(arr, (0, n))
 
-    while stack:
-            a, b = heapq.heappop(stack)
-            if dijk[b] != 10**8:
-                continue
-            dijk[b] = a
-            if 0<=b+1<=100000:
-                heapq.heappush(stack, (a+1, b+1))
-            if 0<=b-1<=100000:
-                heapq.heappush(stack, (a+1, b-1))
-            if 0<=b*2<=100000:
-                heapq.heappush(stack, (a, b*2))
-                
-    print(dijk[m])
+    while arr:
+        a, b = heapq.heappop(arr)
+        if dp[b] != 10**9:
+            continue
+        dp[b] = a
+        if 0 <= b+1 <= 100000:
+            heapq.heappush(arr, (a+1, b+1))
+        if 0 <= b-1 <= 100000:
+            heapq.heappush(arr, (a+1, b-1))
+        if 0 <= b*2 <= 100000:
+            heapq.heappush(arr, (a, b*2))
+
+    print(dp[k])
