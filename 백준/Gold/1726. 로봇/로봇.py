16db7091aa1833da7aa97a48be1e5e3f800f9b34
dexma001 <148747163+dexma001@users.noreply.github.com>
2025-05-22 17:03:50 +0900

    [Gold III] Title: 로봇, Time: 68 ms, Memory: 35140 KB -BaekjoonHub

--- 백준/Gold/1726. 로봇/로봇.py
+++ 백준/Gold/1726. 로봇/로봇.py
@@ -24,34 +24,64 @@ answer = 0
 queue = deque([])
 queue.append([start_y, start_x, start_loc])
 
-location_change = [1, 2, 3]
-x_w = [0, 0, 0, 1, 2, 3, -1, -2, -3, 0, 0, 0, 0, 0, 0]
-y_w = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, -1, -2, -3]
-
 while queue:
     for _ in range(len(queue)):
         y, x, l = queue.popleft()
         if y == end_y and x == end_x and l == end_loc:
             print(answer)
             quit()
-
-        for i in range((l % 2), (l % 2) + 2):
-            changed_location = (l+location_change[i]) % 4
-            if not changed_location:
-                changed_location = 4
-
-            if visited[y][x][changed_location] != 1:
-                visited[y][x][changed_location] = 1
-                queue.append([y, x, changed_location])
-
-        for j in range(l*3, l*3+3):
-            dy = y + y_w[j]
-            dx = x + x_w[j]
-            if 0 <= dy < n and 0 <= dx < m and arr[dy][dx] != 1:
-                if visited[dy][dx][l] != 1:
-                    visited[dy][dx][l] = 1
-                    queue.append([dy, dx, l])
+        if l == 1 or l == 2:
+            if visited[y][x][3] != 1:
+                visited[y][x][3] = 1
+                queue.append([y, x, 3])
+            if visited[y][x][4] != 1:
+                visited[y][x][4] = 1
+                queue.append([y, x, 4])
+            if l == 1:
+                for i in range(1, 4):
+                    dy = y
+                    dx = x + i
+                    if 0 <= dy < n and 0 <= dx < m and arr[dy][dx] != 1:
+                        if visited[dy][dx][l] != 1:
+                            visited[dy][dx][l] = 1
+                            queue.append([dy, dx, l])
+                    else:
+                        break
             else:
-                break
-
+                for i in range(-1, -4, -1):
+                    dy = y
+                    dx = x + i
+                    if 0 <= dy < n and 0 <= dx < m and arr[dy][dx] != 1:
+                        if visited[dy][dx][l] != 1:
+                            visited[dy][dx][l] = 1
+                            queue.append([dy, dx, l])
+                    else:
+                        break
+        else:
+            if visited[y][x][1] != 1:
+                visited[y][x][1] = 1
+                queue.append([y, x, 1])
+            if visited[y][x][2] != 1:
+                visited[y][x][2] = 1
+                queue.append([y, x, 2])
+            if l == 3:
+                for i in range(1, 4):
+                    dy = y + i
+                    dx = x
+                    if 0 <= dy < n and 0 <= dx < m and arr[dy][dx] != 1:
+                        if visited[dy][dx][l] != 1:
+                            visited[dy][dx][l] = 1
+                            queue.append([dy, dx, l])
+                    else:
+                        break
+            else:
+                for i in range(-1, -4, -1):
+                    dy = y + i
+                    dx = x
+                    if 0 <= dy < n and 0 <= dx < m and arr[dy][dx] != 1:
+                        if visited[dy][dx][l] != 1:
+                            visited[dy][dx][l] = 1
+                            queue.append([dy, dx, l])
+                    else:
+                        break
     answer += 1
