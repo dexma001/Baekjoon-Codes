ef24e06cbbebfd188dc858f4fddd54dbf894c237
dexma001 <148747163+dexma001@users.noreply.github.com>
2025-09-17 14:53:12 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void){
+    long N;
+    scanf("%ld", &N);
+
+    for (int i = 0; i < N; i++){
+        printf("%c", 'a');
+    }
+
+}
