05679752d011fc0f9e19e9b748ffd35cf16c5d46
dexma001 <148747163+dexma001@users.noreply.github.com>
2025-11-10 20:04:16 +0900

    [Silver I] Title: 신입 사원, Time: 5328 ms, Memory: 220352 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,18 @@
+for _ in range(int(input())):
+    n = int(input())
+    arr = list()
+    for _ in range(n):
+        arr.append(list(map(int, input().split())))
+        
+    arr.sort(key=lambda x: [x[0], x[1]])
+
+    answer = 1
+    mini = arr[0][1]
+    for i in range(1, n):
+        
+        if arr[i][1] < mini:
+            mini = arr[i][1]
+            answer += 1
+        else:
+            continue
+    print(answer)
