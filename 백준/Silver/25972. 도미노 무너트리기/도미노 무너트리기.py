9ef020b8c45a12efe3238fd8e089a6871065c737
dexma001 <148747163+dexma001@users.noreply.github.com>
2026-01-08 23:12:56 +0900

    [Silver IV] Title: 도미노 무너트리기, Time: 2056 ms, Memory: 250448 KB -BaekjoonHub

---
+++ 백준/Silver/25972. 도미노 무너트리기/도미노 무너트리기.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = list()
+
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+    
+arr.sort(key=lambda x:[x[0], x[1]])
+answer = 0
+
+for i in range(n):
+    if i == 0:
+        answer += 1
+    else:
+        if arr[i-1][0] + arr[i-1][1] >= arr[i][0]:
+            continue
+        else:
+            answer += 1
+            
+print(answer)
