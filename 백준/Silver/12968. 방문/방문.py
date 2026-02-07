651c2d9ba2383b41fe02f7f9b80b27f8539cafd4
dexma001 <148747163+dexma001@users.noreply.github.com>
2026-02-07 17:29:35 +0900

    [Silver I] Title: 방문, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12968. 방문/방문.py
+++ 백준/Silver/12968. 방문/방문.py
@@ -1,17 +1,11 @@
 r, c, k = map(int, input().split())
 
-answer = -1
+answer = 1
 
 if r%2 == 1:
     if c % 2 == 1:
-        if k == 1:
-            answer = 1
-        else:
+        if k > 1:
             answer = 0
-    else:
-        answer = 1
-else:
-    answer = 1
-    
+ 
 print(answer) 
         
